c95924626c284c27ae0d71443e7e9155e241e7f3
Brad <bradleyhersey00@gmail.com>
2018-06-03 21:56:03 -0400

    1.1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
 
-    var topics = ["DragonBallz", "DragonBall Super", "Fairy Tail", "My Hero Academia", "Tokyo Ghoul"];
+    var topics = ["DragonBallz", "DragonBall Super", "Fairy Tail", "My Hero Academia", "Tokyo Ghoul","Bleach","One Piece","Death Note"];
 
 
     function displayAnime() {
+        $('.anime').empty();
+
         var topic = $(this).attr("data");
         var queryURl = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&limit=10&offset=0&rating=R&api_key=qCHU3WJfRnuaeWrO8zjwX5qq3CVgRF3x";
 
@@ -15,16 +17,25 @@ $(document).ready(function () {
 
             console.log(queryURl);
 
-            var animeDiv = $('<div class="anime">');
+
             var search = response.data;
-            for(var i = 0;i<search.length;i++){
-            var rateView = $("<p>").text("Rating bro: " + search[i].rating);
-            animeDiv.append(rateView);
-            var imgURL =search[i].images.fixed_height.url;//check the queryURL 
-            var image = $('<img>').attr("src", imgURL);
-            animeDiv.append(image);
-            $('#gifView').prepend(animeDiv);
-            console.log(search);
+            for (var i = 0; i < search.length; i++) {
+                var animeDiv = $('<div class="anime">');
+                var rateView = $("<p>").text("Rating bro: " + search[i].rating);
+                animeDiv.append(rateView);
+                var imgURL = search[i].images.fixed_height.url;//check the queryURL 
+                var img_url = search[i].images.fixed_height_still.url;
+                var image = $('<img>').attr("src", imgURL);
+
+
+                console.log(search);
+                image.attr("src", img_url);
+                image.addClass(".gif");
+                image.attr("data-state", "still");
+                image.attr("data-still", img_url);
+                image.attr("data-animate", imgURL);
+                animeDiv.append(image);
+                $('#gifView').append(animeDiv);
             }
         });
     }
@@ -37,6 +48,7 @@ $(document).ready(function () {
             b.attr("data", topics[i]);
             b.text(topics[i]);
             $("#btnView").append(b);
+           
         }
     }
     $("#addGif").on('click', function (event) {
@@ -44,11 +56,15 @@ $(document).ready(function () {
         var topic = $('#gifInput').val().trim();
         topics.push(topic);
         renderButton();
+        
+
     });
     $(document).on('click', '.anime-btn', displayAnime);
+   
     renderButton();
-
-    $(".gif").on("click", function () {
+    
+   
+       $(document).on('click',"img",function(){
 
         var state = $(this).attr("data-state");
         if (state === "still") {
@@ -57,8 +73,8 @@ $(document).ready(function () {
         } else {
             $(this).attr("src", $(this).attr('data-still'));
             $(this).attr("data-state", "still");
-
+               
         }
-
-    });
+});
+   
 });
